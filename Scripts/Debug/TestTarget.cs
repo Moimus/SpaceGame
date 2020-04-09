1692367993548baaeb173fb9994db23149c3e3e3
Moimus <33897774+Moimus@users.noreply.github.com>
2020-04-09 16:12:11 +0200

    added explosion effect & IDestructable interface
    IDestructible is implemented by destructible objects

---
+++ Scripts/Debug/TestTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTarget : MonoBehaviour, IHitable, IDestructable
+{
+    public GameObject explosionFx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onDestroy()
+    {
+        GameObject explosion = Instantiate(explosionFx, transform.position, transform.rotation);
+    }
+
+    public void onHit()
+    {
+        onDestroy();
+    }
+}
---
+++ Scripts/FX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FX : MonoBehaviour
+{
+    public float lifeTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
