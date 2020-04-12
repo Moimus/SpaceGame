054385adabf7240bb0a786e9c21dce562d45b454
Moimus <33897774+Moimus@users.noreply.github.com>
2020-04-12 14:49:02 +0200

    added energy powerUp

---
+++ Scripts/Collectable/PowerUps/EnergyPowerUp.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyPowerUp : MonoBehaviour, ICollectable, IDestructable
+{
+    public int energyAmount = 100;
+    public GameObject explosionFx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Ship>() != null)
+        {
+            onCollect(other.GetComponent<Ship>());
+        }
+    }
+
+    public void onCollect(Ship collector)
+    {
+        if (collector.energyCurrent < collector.energyMax)
+        {
+            collector.energyCurrent += energyAmount;
+            if (collector.energyCurrent > collector.energyMax)
+            {
+                collector.energyCurrent = collector.energyMax;
+            }
+            onDestroy();
+        }
+    }
+
+    public void onDestroy()
+    {
+        GameObject explosion = Instantiate(explosionFx, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
