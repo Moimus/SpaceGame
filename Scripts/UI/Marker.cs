bc02d7474bec28436dfdefcdd22d14f6fc3119c5
Moimus <33897774+Moimus@users.noreply.github.com>
2020-04-27 10:15:00 +0200

    Added distance label to targetmarker

--- Scripts/UI/Marker.cs
+++ Scripts/UI/Marker.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Marker : MonoBehaviour
 {
     public Ship uiOwner;
     public Transform markedObject;
+    public Text distanceText;
     RectTransform rectTransform;
     public float uniformScale = 1;
     int screenWidthHalf = Screen.width / 2;
@@ -38,6 +40,7 @@ public class Marker : MonoBehaviour
             Debug.Log(targetRelativePos.ToString());
             Vector2 localPoint;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponent<RectTransform>(), targetRelativePos, uiOwner.mainCamera, out localPoint);
+            updateDistanceText();
             if(localPoint.x > -screenWidthHalf && localPoint.x < screenWidthHalf && localPoint.y > -screenHeightQuarter && localPoint.y < screenHeightQuarter && targetRelativePos.z > 0)
             {
                 rectTransform.localPosition = new Vector3(localPoint.x, localPoint.y, uiOwner.mainCamera.nearClipPlane + 0.1f);
@@ -56,5 +59,13 @@ public class Marker : MonoBehaviour
         StopCoroutine(lifeCycle());
         Destroy(gameObject);
         yield return null;
+    }
+
+    void updateDistanceText()
+    {
+        float distance = Vector3.Distance(markedObject.position, uiOwner.transform.position * 10);
+        string distanceString = distance.ToString();
+        string text = distanceString.Substring(0, distanceString.Length - 3);
+        distanceText.text = text;
     }
 }
