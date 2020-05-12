fa89bf096d69a855bcb85f527009e75fc6b8f931
Moimus <33897774+Moimus@users.noreply.github.com>
2020-05-12 10:03:28 +0200

    dbg

---
+++ Scripts/Debug/AimDebug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimDebug : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform.position);
+        transform.Translate(Vector3.forward * Time.deltaTime * 5);
+    }
+}
